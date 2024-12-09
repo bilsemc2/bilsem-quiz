51fe87383ca497eab5d5de96c4d38f4f8b3eb7a5
bilsemc2 <yetenekvezeka@yetenek-MacBook-Pro.local>
2024-12-09 09:41:20 +0300

    Move Supabase credentials to environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://biltdannugeaosqgjryo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpbHRkYW5udWdlYW9zcWdqcnlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1NzIwNjgsImV4cCI6MjA0OTE0ODA2OH0.-KXgbwXpB0KD6dpmQEhmzfg7v-6XdRzrGz7DOnKMflM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
