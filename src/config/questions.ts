b3cdf746c01ce17d46e3df5d62f1f30d753f143b
bilsemc2 <bilsemc2.eth@ethermail.io>
2024-12-20 12:15:24 +0300

    Your commit message here

--- src/config/questions.ts
+++ src/config/questions.ts
@@ -1,8 +1,8 @@
 export const QUESTIONS_CONFIG = {
-  totalQuestions: 140, // Toplam soru sayısı
+  totalQuestions: 170, // Toplam soru sayısı
   categories: {
     Matris: {
-      totalQuestions: 140,
+      totalQuestions: 170,
       path: 'images/questions/Matris'
     }
   }
