c66477f7b43fc488ceeae77cd96eab2139b00c19
bilsemc2 <bilsemc2.eth@ethermail.io>
2025-01-08 11:41:34 +0300

    msg

--- src/config/questions.ts
+++ src/config/questions.ts
@@ -1,54 +1,9 @@
 export const QUESTIONS_CONFIG = {
-  totalQuestions: 407, // Toplam soru sayısı
+  totalQuestions: 418, // Toplam soru sayısı
   categories: {
     Matris: {
-      totalQuestions: 407,
+      totalQuestions: 418,
       path: '/images/questions/Matris'
     }
   }
-};
-
-export const generateQuiz = () => {
-  const questions = [];
-  const category = QUESTIONS_CONFIG.categories.Matris;
-  const questionNumber = Math.floor(Math.random() * category.totalQuestions) + 1;
-  
-  const options = [
-    {
-      id: 'A',
-      text: 'A',
-      imageUrl: `${category.path}/${questionNumber}/A.png`
-    },
-    {
-      id: 'B',
-      text: 'B',
-      imageUrl: `${category.path}/${questionNumber}/B.png`
-    },
-    {
-      id: 'C',
-      text: 'C',
-      imageUrl: `${category.path}/${questionNumber}/C.png`
-    },
-    {
-      id: 'D',
-      text: 'D',
-      imageUrl: `${category.path}/${questionNumber}/D.png`
-    },
-    {
-      id: 'E',
-      text: 'E',
-      imageUrl: `${category.path}/${questionNumber}/E.png`
-    }
-  ];
-
-  questions.push({
-    id: questionNumber,
-    text: `Soru ${questionNumber}`,
-    questionImageUrl: `${category.path}/${questionNumber}/Q.png`,
-    options,
-    correctOptionId: 'A', // TODO: Doğru cevapları ekle
-    solutionVideo: `${category.path}/${questionNumber}/solution.mp4`
-  });
-
-  return { questions };
 };
