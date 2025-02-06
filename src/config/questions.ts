1b0549624c25806d52c4b476317fd63a6dfed31e
bilsemc2 <bilsemc2.eth@ethermail.io>
2025-02-06 11:15:30 +0300

    msg

--- src/config/questions.ts
+++ src/config/questions.ts
@@ -1,8 +1,8 @@
 export const QUESTIONS_CONFIG = {
-  totalQuestions: 1124, // Toplam soru sayısı
+  totalQuestions: 1154, // Toplam soru sayısı
   categories: {
     Matris: {
-      totalQuestions: 1124,
+      totalQuestions: 1154,
       path: '/images/questions/Matris'
     }
   }
