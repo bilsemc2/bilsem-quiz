f123bc85440c5d4d3a376329469da3239dab1715
bilsemc2 <bilsemc2.eth@ethermail.io>
2025-03-02 09:52:37 +0300

    msg

--- src/constants/meetCodes.ts
+++ src/constants/meetCodes.ts
@@ -1,7 +1,7 @@
 // Her sınıf için sabit Google Meet kodları
 export const MEET_CODES = {
     // Sınıf UUID'leri ve Meet kodları
-    '4e568d77-8465-44b8-b1dd-1077a7361fbc': 'yin-ncbz-bis',  // 1. Sınıf
-    'd17a606f-e6f9-4b27-a7b0-bfa76f58115b': 'xac-zztn-dnt',  // 2. Sınıf
-    '29dc0da2-1a79-422d-810c-14b784d106ba': 'gwy-qznu-xpx',  // 3. Sınıf
+    '4e568d77-8465-44b8-b1dd-1077a7361fbc': '',  // 1. Sınıf
+    'd17a606f-e6f9-4b27-a7b0-bfa76f58115b': '',  // 2. Sınıf
+    '29dc0da2-1a79-422d-810c-14b784d106ba': '',  // 3. Sınıf
 };
