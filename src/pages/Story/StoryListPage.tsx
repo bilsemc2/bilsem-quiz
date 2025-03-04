dfa483752fb9b95016fd9a61a7b7b6db53a7b9e9
bilsemc2 <bilsemc2.eth@ethermail.io>
2025-03-04 12:50:58 +0300

    msg

--- src/pages/Story/StoryListPage.tsx
+++ src/pages/Story/StoryListPage.tsx
@@ -1,13 +1,16 @@
 // React componenti
 import { StoriesList } from './components/StoriesList';
+import RequireAuth from '../../components/RequireAuth';
 
 export default function StoryListPage() {
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-4xl font-bold text-center text-purple-900 mb-12">
-        Hikayeler
-      </h1>
-      <StoriesList />
-    </div>
+    <RequireAuth>
+      <div className="container mx-auto py-8">
+        <h1 className="text-4xl font-bold text-center text-purple-900 mb-12">
+          Hikayeler
+        </h1>
+        <StoriesList />
+      </div>
+    </RequireAuth>
   );
 }
